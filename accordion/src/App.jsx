9155e5b9a72da2cb6033ff32319d7d74d05f186e
siamso <hello.siamahamed@gmail.com>
2024-07-13 14:15:18 +0600

    finished all the small projects

--- accordion/src/App.jsx
+++ accordion/src/App.jsx
@@ -48,7 +48,7 @@ function App() {
       <UseOutsideClickTest />
       <UseWindowResizeTest /> */}
       {/* <ScrollTopToBottom /> */}
-      <ScrollToSection />
+      {/* <ScrollToSection /> */}
     </div>
   );
 }
